77da9d4f2da0703e7f14b568c0fc463715d0dcca
cesar.romero <cesar.romero@commonms.com>
2016-12-14 20:34:35 +0100

    seems ok, but it is not, f*****ck!!!

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <functional>
 #include <iostream>
 #include <iterator>
+#include <map>
 #include <numeric>
 #include <sstream>
 #include <utility>
@@ -35,6 +36,30 @@ int partition(Vector & v, int first, int last)  //  inclusive
     return i - 1;
 }
 
+int getPivotIndex(Vector & v, int first, int last)
+{
+    int size = last - first + 1;
+    if (size < 3)
+    {
+        return v[first] < v[last] ? first : last;
+    }
+    int middle;
+    if (size % 2 == 0)
+    {
+        middle = size / 2 - 1;
+    }
+    else
+    {
+        middle = size / 2;
+    }
+    int f = v[first],
+        m = v[middle],
+        l = v[last];
+    map<int,int> values{ make_pair(f, first), make_pair(m, middle), make_pair(l, last) };
+    int result = (++begin(values))->second;
+    return result;
+}
+
 int comparisons = 0;
 
 void quicksort(Vector & v, int first, int last) //  inclusive
@@ -44,7 +69,9 @@ void quicksort(Vector & v, int first, int last) //  inclusive
         return;
     }
     comparisons += last - first;
-    swap(v[first], v[last]);    //  for the last!!!!
+//    swap(v[first], v[last]);    //  for the last!!!!
+    int pivotIndex = getPivotIndex(v, first, last);
+    swap(v[first], v[pivotIndex]);    //  for the median!!!!
     int p = partition(v, first, last);
     quicksort(v, first, p - 1);
     quicksort(v, p + 1, last);
@@ -76,8 +103,8 @@ Vector read()
 
 int main()
 {
-//    Vector v = { -1, 0, -2, 1 };
-    Vector v = read();
+    Vector v = { 5, 4, 3, 2, 1, 0 };
+//    Vector v = read();
     int size = v.size();
     quicksort(v, 0, size - 1);
     cout << "number of comparisons: " << comparisons << endl;
