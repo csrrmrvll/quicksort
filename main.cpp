d5751ac9f41763528ad6c166ac33713ab6a39859
cesar.romero.avello <cesar.romero.avello@gmail.com>
2017-01-29 15:25:09 +0100

    code cleaning

--- main.cpp
+++ main.cpp
@@ -20,56 +20,66 @@ void output(Vector v)
     cout << endl;
 }
 
-int partition(Vector & v, int first, int last)  //  inclusive
+class QuickSort
 {
-    const int pivot = v[first];
-    int i = first + 1;
-    for (int j = i; j <= last; ++j)
+public:
+    int sortAndGetComparisons(Vector & v)
     {
-        if (v[j] < pivot)
+        int comparisons = 0;
+        this->quicksort(v, 0, v.size() - 1, comparisons);
+        return comparisons;
+    }
+
+private:
+    void quicksort(Vector & v, int first, int last, int & comparisons) //  inclusive
+    {
+        if (first >= last)
         {
-            swap(v[i], v[j]);
-            ++i;
+            return;
         }
+        const int size = last - first + 1;
+        comparisons += size - 1;
+        int pivotIndex = first;
+    //        int pivotIndex = last;
+    //        int pivotIndex = getPivotIndex(v, first, last);
+        swap(v[first], v[pivotIndex]);    //  for the median!!!!
+        pivotIndex = partition(v, first, last);
+        quicksort(v, first, pivotIndex - 1, comparisons);
+        quicksort(v, pivotIndex + 1, last, comparisons);
     }
-    swap(v[first], v[i - 1]);
-    return i - 1;
-}
 
-int getPivotIndex(Vector & v, int first, int last)
-{
-    int size = last - first + 1;
-    if (size < 3)
+    int partition(Vector & v, int first, int last)  //  inclusive
     {
-        return v[first] < v[last] ? first : last;
+        const int pivot = v[first];
+        int i = first + 1;
+        for (int j = i; j <= last; ++j)
+        {
+            if (v[j] < pivot)
+            {
+                swap(v[i], v[j]);
+                ++i;
+            }
+        }
+        swap(v[first], v[i - 1]);
+        return i - 1;
     }
-    const int   middle = (first + last) / 2,
-                f = v[first],
-                m = v[middle],
-                l = v[last];
-    const map<int,int> values{ make_pair(f, first), make_pair(m, middle), make_pair(l, last) };
-    const int result = (++begin(values))->second;
-    return result;
-}
 
-int comparisons = 0;
-
-void quicksort(Vector & v, int first, int last) //  inclusive
-{
-    if (first >= last)
+    int getPivotIndex(Vector & v, int first, int last)
     {
-        return;
+        int size = last - first + 1;
+        if (size < 3)
+        {
+            return v[first] < v[last] ? first : last;
+        }
+        const int   middle = (first + last) / 2,
+                    f = v[first],
+                    m = v[middle],
+                    l = v[last];
+        const map<int,int> values{ make_pair(f, first), make_pair(m, middle), make_pair(l, last) };
+        const int result = (++begin(values))->second;
+        return result;
     }
-    const int size = last - first + 1;
-    comparisons += size - 1;
-    const int pivotIndex = first;
-//        int pivotIndex = last;
-//        int pivotIndex = getPivotIndex(v, first, last);
-    swap(v[first], v[pivotIndex]);    //  for the median!!!!
-    const int p = partition(v, first, last);
-    quicksort(v, first, p - 1);
-    quicksort(v, p + 1, last);
-}
+};
 
 Vector read()
 {
@@ -101,10 +111,10 @@ int main()
 //    Vector v = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
 //    Vector v = { 3, 9, 8, 4, 6, 10, 2, 5, 7, 1 };
     Vector v = read();
-    const int size = v.size();
-    quicksort(v, 0, size - 1);
+    QuickSort qs;
+    const int comparisons = qs.sortAndGetComparisons(v);
     cout << "number of comparisons: " << comparisons << endl;
-    Vector sorted(size);
+    Vector sorted(v.size());
     int min = *min_element(begin(v), end(v));
     iota(sorted.begin(),sorted.end(),min);
     if (v != sorted)
